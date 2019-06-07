4e6cb88bdfe747409abfadf71e89ae48de572a08
unknown <Administrador@MULTIPLA.LOCAL>
2019-06-07 11:56:34 -0300

    router system.

---
+++ core/Router.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Core;
+
+class Router{
+
+    private $rotas_validas = [];
+
+
+    private function val_method($method){
+
+    }
+
+
+    public function __callStatic($method,$arg){
+        
+    }
+
+
+
+
+}
