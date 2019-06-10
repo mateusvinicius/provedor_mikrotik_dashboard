a59f72d24ddbfb59825e31b8557f1b3658f4134f
Mateus Vinicius <mateusvinicios7@gmail.com>
2019-06-10 15:15:18 -0300

    add method verificar_controller and function view

--- core/Controller.php
+++ core/Controller.php
@@ -5,10 +5,22 @@ class Controller {
 
 
     protected final function view(string $name,array $var = []){
-        $_view = __DIR__."";
+        try{
+            $_view = __DIR__."/../App/View/".$name."php";
+            if(!file_exists($_view))
+                throw \Exception("View não existe..",1);
+    
+           include $_view;     
 
+        }catch(\Exception $e){
+
+            return $e->getMessage();
+        }
+     
 
     }
+
+    
 
 
 }
--- core/Router.php
+++ core/Router.php
@@ -5,6 +5,7 @@ namespace Core;
 class Router{
 
     private $rotas_validas = [];
+    private  static $parametros = [];
 
 
     private function val_method($method){
@@ -14,18 +15,21 @@ class Router{
     }
 
 
+
     public function __callStatic($method,$arg){
         try{
 
             if(!self::val_method($method))
                 throw new \Exception("Method not valid",1);
 
-                [$router,$controller] = $arg;
+            [$router,$controller] = $arg;
+
+            if(!isset($router) or !isset($controller))
+                throw new \Exception("Router or controller not valid",1);   
 
 
 
 
-             
 
 
         }catch(\Exception $e){
@@ -36,6 +40,15 @@ class Router{
     }
 
 
+
+
+    private function verificar_controller(string $caminho){
+
+         [$_arquivo,$_function] = explode("@",$caminho);
+
+         
+
+    }
 
 
 }
